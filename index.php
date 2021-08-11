3d46cf559e1debf437f0e1c496ebf06ba6d90335
“Michelle <MNguyen000@sea.yearup.org>
2021-08-11 11:24:42 -0700

    Updates data

--- index.php
+++ index.php
@@ -79,14 +79,13 @@ function bondJSON(data){
     $('#films').html('');
 
     //loop through films and add template
-    /*
-    $.each(data.films,function(i,item){
-        let myFilm = bondTemplate(item);
+    
+    $.each(data.games,function(i,item){
+        let myGame = bondTemplate(item);
 
-        $('<div></div>').html(myFilm).appendTo('#films');
+        $('<div></div>').html(myGame).appendTo('#films');
 
     });
-    */
     //$("#output").text(JSON.stringify(data));
 
     //this creates a map of the JSON on our page
@@ -97,23 +96,27 @@ function bondJSON(data){
     
 }
 
-function bondTemplate(film){
+function bondTemplate(game){
 
     return `
         <div class="film">
-            <b>Film: </b>${film.Film}</br>
-            <b>Title: </b>${film.Title}</br>
-            <b>Year: </b>${film.Year}</br>
-            <b>Director: </b>${film.Director}</br>
-            <b>Producers: </b>${film.Producers}</br>
-            <b>Writers: </b>${film.Writers}</br>
-            <b>Composer: </b>${film.Composer}</br>
-            <b>Bond: </b>${film.Bond}</br>
-            <b>Budget: </b>${film.Budget}</br>
-            <b>Box Office:</b>${film.BoxOffice}</br>
-            <div class="pic"><img src="thumbnails/${film.Image}" /></div>
+            <b>Title: </b>${game.Title}</br>
+            <b>Genre: </b>${game.Genre}</br>
+            <b>Company: </b>${game.Company}</br>
+            <b>Year: </b>${game.Year}</br>
+            <b>Rating: </b>${game.Rating}</br>
+            <div class="pic"><img src="thumbnails/${game.Image}" /></div>
         </div>
     `;
+
+/*
+"Title":"Sonic the Hedgehog",
+"Genre":"Platformer",
+"Company":"Sega",
+"Year":1991,
+"Rating":"E",
+"Image":"sonic.jpg"
+*/
 
 }
 
